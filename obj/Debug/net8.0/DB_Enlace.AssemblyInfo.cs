a4455f941a80e29908080048d5aa6ad13560f497
jose.guevara <134351019+JPGuevara37@users.noreply.github.com>
2024-03-08 20:36:32 -0600

    Version.0.29.1

--- obj/Debug/net8.0/DB_Enlace.AssemblyInfo.cs
+++ obj/Debug/net8.0/DB_Enlace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DB_Enlace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abd0f3b5b516d6f57a9c048dfc1c4de8dab40e13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d09d3df6ea6d1f681bfd54c4043dfdb558085f63")]
 [assembly: System.Reflection.AssemblyProductAttribute("DB_Enlace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DB_Enlace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
